2938e5cb6c93fdb8c833df62962192025cc9df8c
Henrique <rkgoulart@hotmail.com>
2026-05-20 12:32:44 +0200

    fix: add production server to Swagger

--- src/swagger.ts
+++ src/swagger.ts
@@ -11,7 +11,12 @@ const options: swaggerJsdoc.Options = {
     },
     servers: [
       {
+        url: "https://ecommerce-api-s8fd.onrender.com",
+        description: "Production",
+      },
+      {
         url: "http://localhost:3000",
+        description: "Development",
       },
     ],
   },
